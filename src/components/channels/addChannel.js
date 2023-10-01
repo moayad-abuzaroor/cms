4d3bbcddf1d4c784e47e818a05ce0368e0a9e786
Moayad AbuZarour <Moayad.AbuZarour@Jawwal.ps>
2023-10-01 12:20:21 +0300

    create select epg modal

--- src/components/channels/addChannel.js
+++ src/components/channels/addChannel.js
@@ -1,10 +1,80 @@
 import React, { useState } from "react";
+import ReactPaginate from "react-paginate";
 import MovieTitleComponent from "../shared/movietTitleComponent";
 import ChannelsTitleComponent from "../shared/channelsTitleComponent";
 import ChannelsNavBar from "../shared/ChannelsNavBar";
+import { faCircleMinus, faList, faMinusCircle, faPersonCircleMinus, faRemove } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 function AddChannel(){
+
+    const [epg, setEPG] = useState([
+        {
+            title: 'Abu Dhabi AE',
+            source: 'EPG best',
+            last_updated: '6/25/23, 11:00 PM',
+            expire: '7/2/23, 12:00 AM',
+            syncing: 'Yes'
+        },
+        {
+            title: '12 Keshet IL',
+            source: 'EPG best',
+            last_updated: '6/25/23, 11:00 PM',
+            expire: '7/2/23, 12:00 AM',
+            syncing: 'Yes'
+        },
+        {
+            title: 'Al Jazeera',
+            source: 'EPG best',
+            last_updated: '6/25/23, 11:00 PM',
+            expire: '7/2/23, 12:00 AM',
+            syncing: 'No'
+        },
+        {
+            title: '12 Keshet IL',
+            source: 'EPG best',
+            last_updated: '6/25/23, 11:00 PM',
+            expire: '7/2/23, 12:00 AM',
+            syncing: 'Yes'
+        },
+        {
+            title: 'Al Jazeera',
+            source: 'EPG best',
+            last_updated: '6/25/23, 11:00 PM',
+            expire: '7/2/23, 12:00 AM',
+            syncing: 'No'
+        },
+        {
+            title: '12 Keshet IL',
+            source: 'EPG best',
+            last_updated: '6/25/23, 11:00 PM',
+            expire: '7/2/23, 12:00 AM',
+            syncing: 'Yes'
+        },
+        {
+            title: 'Al Jazeera',
+            source: 'EPG best',
+            last_updated: '6/25/23, 11:00 PM',
+            expire: '7/2/23, 12:00 AM',
+            syncing: 'No'
+        }
+    ]);
+
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const itemsPerPage = 8; // Adjust as needed
+    const totalPages = Math.ceil(epg.length / itemsPerPage);
+
+    const startIndex = currentPage * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentEPG = epg.slice(startIndex, endIndex);
+
+    const [selectedEPG, setSelectedEPG] = useState('');
+    const handleTDClick = (title) => {
+        setSelectedEPG(title);
+        closeModal();
+    };
 
     const [title, setTitle] = useState('');
     const [requiredMsg, setRequiredMsg] = useState(false);
@@ -85,6 +155,16 @@ function AddChannel(){
         }
     }
 
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const openModal = () => {
+        setModalVisible(true);
+    }
+
+    const closeModal = () => {
+        setModalVisible(false);
+    }
+
     return(
         <div className="container-fluid bg-light" style={{ padding: '2%', height: '100%'}}>
             <div className='row'>
@@ -115,7 +195,15 @@ function AddChannel(){
 
                         <div className='form-group col-md-4'>
                             <label className='labelBox'>Electronic Program Guide</label>
-                            <input className='form-control' disabled/>
+                            <div style={{display: 'flex'}}>
+                                <input value={selectedEPG} className='form-control customBorderRight' disabled/>
+                                <button type="button" className="btn btn-primary customBorderLeft customBorderRight" onClick={openModal}>
+                                    <FontAwesomeIcon className='mx-1' icon={faList} />
+                                </button>
+                                <button type="button" className="btn btn-primary customBorderLeft" onClick={() => {setSelectedEPG('')}}>
+                                    <FontAwesomeIcon icon={faCircleMinus} />
+                                </button>
+                            </div>                            
                         </div>
 
                         <div className='form-group col-md-4'>
@@ -190,6 +278,95 @@ function AddChannel(){
                     </div>                    
                 </form>
             </div>
+
+
+            {modalVisible &&
+                <div className="custom-modal">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h6 className="modal-title">Select Electronic Program Guide</h6>
+                            <button type="button" className="close" onClick={closeModal} aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            {/* Modal content goes here */}
+                            <div className="form-row mt-1">
+                                <div className="form-group col-md-4">
+                                    <label className="labelBox">
+                                        EPG Source
+                                    </label>
+                                    <select
+                                        className='form-control'
+                                        name="epgsource"
+                                    >
+                                        <option selected="false" disabled="disabled">
+                                        Select EPG Source
+                                        </option>
+                                        <option>EPG best</option>
+                                    </select>
+                                </div>
+                                <div className="form-group col-md-4">
+                                    <label className="labelBox">Title</label>
+                                    <input
+                                        className='form-control'
+                                        type="text"
+                                    />
+                                </div>
+                                <div className="mt-2 ml-1">
+                                    <button type="button" className="btn btn-primary mt-4" >Search</button>
+                                </div>
+                            </div>
+                            <div className="table-responsive">
+                                <table className="table table-striped">
+                                    <thead className="thead-dark">
+                                        <tr>
+                                            <th className="text-center align-middle">Title</th>
+                                            <th className="text-center align-middle">EPG Source</th>
+                                            <th className="text-center align-middle">Last Updated</th>
+                                            <th className="text-center align-middle">EPG Expire</th>
+                                            <th className="text-center align-middle">Syncing</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {currentEPG.map((ep, index) => (
+                                            <tr className="text-center align-items-center" key={index}>
+                                                <td className='align-middle'>{ep.title}</td>
+                                                <td className='align-middle'>{ep.source}</td>
+                                                <td className='align-middle'>{ep.last_updated}</td>
+                                                <td className='align-middle'>{ep.expire}</td>
+                                                <td className='align-middle' onClick={() => handleTDClick(ep.title)} style={{cursor: 'pointer'}}>
+                                                    <span className={ep.syncing === 'Yes' ? 'badge badge-success custom_white' : 'badge badge-danger custom_white'}>
+                                                        {ep.syncing}
+                                                    </span>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className="row justify-content-center mt-4">
+                                <div className="col-auto">
+                                    <ReactPaginate
+                                        pageCount={totalPages}
+                                        pageRangeDisplayed={0} // Adjust as needed
+                                        marginPagesDisplayed={0} // Adjust as needed
+                                        onPageChange={({ selected }) => setCurrentPage(selected)}
+                                        containerClassName={'pagination'}
+                                        activeClassName={'active'}
+                                        previousLabel={'Previous'} // Add previous label
+                                        nextLabel={'Next'} // Add next label
+                                        disableInitialCallback // Disable the initial callback to prevent multiple renders
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
+
+
+
         </div>
     );
 
